22cfde41840e183875b293f10f7ec5862fcf4117
kristenfoshay <kristenfoshay@gmail.com>
2022-02-16 19:49:36 -0500

    modifying app.py and adding users folder

--- app.py
+++ app.py
@@ -12,3 +12,42 @@ toolbar = DebugToolbarExtension(app)
 def homepage():
 
     return render_template('home-anon.html')
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    """Handle user signup.
+
+    Create new user and add to DB. Redirect to home page.
+
+    If form not valid, present form.
+
+    If the there already is a user with that username: flash message
+    and re-present form.
+    """
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+    form = UserAddForm()
+    
+
+    if form.validate_on_submit():
+        try:
+            user = User.signup(
+                username=form.username.data,
+                email=form.email.data,
+                location=form.location.data,
+                image_url=form.image_url.data or User.image_url.default.arg,
+                password=form.password.data,
+                
+            )
+            db.session.commit()
+
+        except IntegrityError as e:
+            flash("Username already taken", 'danger')
+            return render_template('users/signup.html', form=form)
+
+        do_login(user)
+
+        return redirect("/")
+
+    else:
+        return render_template('users/signup.html', form=form)
