f22ff0f6a82a9ed69ab6afe0fcb46755cd857c9d
kristenfoshay <kristenfoshay@gmail.com>
2022-03-16 15:33:10 -0400

    editing

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ CURR_USER_KEY = "curr_user"
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///flask-heroku')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///ranger')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
